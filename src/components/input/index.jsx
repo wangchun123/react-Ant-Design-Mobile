05ea9d02af6d49522ff0646d69e035f06341dfc5
qw <123@163.com>
2020-09-21 17:07:31 +0800

    :construction: add-new-components

---
+++ src/components/input/index.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Classnames from 'classnames';
+
+import './index.scss';
+
+class Input extends Component {
+  static defaultProps = {
+    onChange: () => null,
+    type: 'text',
+    placeholder: '',
+    disabled: false,
+    value: '',
+    className: '',
+    maxLength: 1000000,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: this.props.value,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.value !== nextProps.value) {
+      this.setState({
+        value: nextProps.value,
+      });
+    }
+  }
+
+  _onChange = (val) => {
+    this.setState({
+      value: val,
+    });
+  };
+
+  render() {
+    const {
+      onChange,
+      type,
+      placeholder,
+      disabled,
+      className,
+      maxLength,
+    } = this.props;
+    const { value } = this.state;
+
+    const myInputClassName = Classnames('my-input-content', className);
+
+    return (
+      <div className="my-input">
+        <input
+          value={value}
+          disabled={disabled && 'disabled'}
+          maxLength={maxLength}
+          placeholder={placeholder}
+          type={type}
+          className={myInputClassName}
+          onChange={(e) => {
+            onChange && onChange(e.target.value);
+            this._onChange(e.target.value);
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+Input.propTypes = {
+  onChange: PropTypes.func,
+  type: PropTypes.oneOf(['text', 'password']),
+  placeholder: PropTypes.string,
+  disabled: PropTypes.bool,
+  value: PropTypes.string,
+  className: PropTypes.string,
+  maxLength: PropTypes.number,
+};
+
+export default Input;
--- src/components/verificationCode/index.jsx
+++ src/components/verificationCode/index.jsx
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import Input from '@/components/input';
 import './index.scss';
 
 export default class index extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div className="verCode">
+          <Input />
+        </div>
+      </div>
+    );
   }
 }
