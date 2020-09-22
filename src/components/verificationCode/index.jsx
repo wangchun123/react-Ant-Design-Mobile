b2fa7ceecdf9f26756023f349c00052489856f1c
qw <123@163.com>
2020-09-22 15:41:57 +0800

    :sparkles: add-new-function

--- src/components/verificationCode/index.jsx
+++ src/components/verificationCode/index.jsx
@@ -1,45 +1,82 @@
 import React, { Component } from 'react';
 import Input from '@/components/input';
+import { Modal } from 'antd-mobile';
 import './index.scss';
+
+const alert = Modal.alert;
 
 export default class index extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      disabled: false,
       text: '获取验证码',
-      num: 20,
+      num: 60,
+      activeClass: '',
+      value: '',
     };
   }
 
   _onClick = () => {
+    if (this.checkError()) return;
+
     let { num } = this.state;
-    console.log('num', num);
+    this.timer = setInterval(() => {
+      this.setState(
+        {
+          text: `${num}s`,
+          num: num--,
+          activeClass: 'activeClass',
+        },
+        () => {
+          if (this.state.num < 1) {
+            clearInterval(this.timer);
+            this.setState({
+              text: '获取验证码',
+              activeClass: '',
+              num: 60,
+            });
+          }
+        },
+      );
+    }, num * 10);
+  };
 
-    this.timer = setTimeout(() => {
-      console.log('--pp--', num--);
+  checkError = () => {
+    const { value } = this.state;
 
-      this.setState({
-        text: `${num--}s`,
-      });
-    }, 1000);
+    if (!value) {
+      alert('错误', '电话不能为空');
+      return true;
+    } else if (!/^1[3456789]\d{9}$/.test(value)) {
+      alert('错误', '电话输入不正确');
+      return true;
+    }
+
+    return false;
+  };
+
+  _onChange = (val) => {
+    this.setState({
+      value: val,
+    });
   };
 
   render() {
-    const { disabled, text } = this.state;
+    const { text, activeClass } = this.state;
     return (
       <div>
         <div className="verCode">
           <Input
-            onChange={(val) => console.log('val', val)}
-            value="qweqweqw"
-            disabled={disabled}
-            maxLength={10}
+            onChange={(val) => this._onChange(val)}
+            maxLength={11}
             trim={true}
             className="cln-input"
           />
-          <div className="my-button" onClick={() => this._onClick()}>
+          <div
+            className={`${'my-button'} ${activeClass}`}
+            onClick={() => this._onClick()}
+          >
             {text}
           </div>
         </div>
